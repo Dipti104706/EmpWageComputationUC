669858ad76271e2472e947e6c29d51ef71803737
Dipti104706 <diptimayeebehura104@gmail.com>
2021-08-28 00:21:55 +0530

    Refactored: Code to compute employee wage using function

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,31 +1,40 @@
 #!/bin/bash -x
 
-isParttime=1;
-isFulltime=2;
-maxHrsinMonth=100;
-empRatePerHour=20;
-numWorkingDays=20;
+#CONSTANTS FOR THE PROGRAM
+IS_PART_TIME=1;
+IS_FULL_TIME=2;
+MAX_HRS_IN_MONTH=100;
+EMP_RATE_PER_HR=20;
+NUM_WORKING_DAYS=20;
+
+#VARIABLES
 totalEmpHrs=0;
 totalWorkingDays=0;
 
-while [[ totalEmpHrs -lt $maxHrsinMonth &&
-         totalWorkingDays -lt numWorkingDays ]]
+function getWorkHrs() {
+   case $1 in
+      $IS_FULL_TIME)
+         empHrs=8
+         ;;
+      $IS_PART_TIME)
+         empHrs=4
+         ;;
+      *)
+         empHrs=0
+         ;;
+   esac
+   echo $empHrs
+}
+
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH &&
+         $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
-   ((numWorkingDays++))
-   empcheck=$((RANDOM%3));
-      case $empcheck in
-         $isFulltime)
-               empHrs=8
-               ;;
-         $isFulltime)
-               empHrs=4
-               ;;
-         *)
-         empHrs=0;
-      esac
-      totalEmpHrs=$(($totalEmpHrs+$empHrs))
+   ((totalWorkingDays++))
+   empHrs="$( getWorkHrs $((RANDOM%3)) )"
+   totalEmpHrs=$(($totalEmphrs+$empHrs))
 
 done
 
-totalsalary=$(($totalEmpHrs+$maxHrsinMonth))
+totalSalary=$(($totalEmpHrs+$EMP_RATE_PER_HR));
+
 
